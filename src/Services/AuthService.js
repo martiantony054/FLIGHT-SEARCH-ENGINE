170a0d74286b153a89179655ef79f0894c332bb0
Martin Antony <martinantony054@gmail.com>
2026-02-05 01:33:03 +0530

    updated on 5 feb

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -8,7 +8,7 @@ export const getAccessToken = async () => {
   }
 
   try {
-    const response = await fetch("/api/v1/security/oauth2/token", {
+    const response = await fetch("/api/auth", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
