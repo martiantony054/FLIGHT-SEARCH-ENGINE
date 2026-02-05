c52a68627baf2c24adee89999fed131d38af631c
Martin Antony <martinantony054@gmail.com>
2026-02-05 10:12:58 +0530

    updated on 5 feb

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -1,38 +1,41 @@
+// export const getAccessToken = async () => {
+//   try {
+//     const response = await fetch("/api/v1/security/oauth2/token", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//       body: new URLSearchParams({
+//         grant_type: "client_credentials",
+//         client_id: import.meta.env.VITE_AMADEUS_CLIENT_ID,
+//         client_secret: import.meta.env.VITE_AMADEUS_CLIENT_SECRET,
+//       }),
+//     });
+
+//     if (!response.ok) {
+//       const errorData = await response.json();
+//       console.error(" Amadeus Error Details:", errorData);
+
+//       throw new Error(
+//         `Auth Failed (${response.status}): ${errorData.error_description || errorData.error}`,
+//       );
+//     }
+
+//     const data = await response.json();
+//     return data.access_token;
+//   } catch (error) {
+//     console.error("Network or Fetch Error:", error);
+//     throw error;
+//   }
+// };
+
 export const getAccessToken = async () => {
-  const apiKey = import.meta.env.VITE_AMADEUS_CLIENT_ID;
-  const apiSecret = import.meta.env.VITE_AMADEUS_CLIENT_SECRET;
+  const response = await fetch("/api/auth");
 
-  if (!apiKey || !apiSecret) {
-    console.error(" Missing credentials in .env file");
-    throw new Error("Missing API credentials.");
+  if (!response.ok) {
+    throw new Error("Failed to fetch access token");
   }
 
-  try {
-    const response = await fetch("/v1/security/oauth2/token", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        grant_type: "client_credentials",
-        client_id: apiKey,
-        client_secret: apiSecret,
-      }),
-    });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      console.error(" Amadeus Error Details:", errorData);
-
-      throw new Error(
-        `Auth Failed (${response.status}): ${errorData.error_description || errorData.error}`,
-      );
-    }
-
-    const data = await response.json();
-    return data.access_token;
-  } catch (error) {
-    console.error("Network or Fetch Error:", error);
-    throw error;
-  }
+  const data = await response.json();
+  return data.token;
 };
